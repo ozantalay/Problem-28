b1778648143e7886367ef3501aa1ff9c1b649c86
ozantalay <ozntalay@hotmail.com>
2024-09-16 03:02:12 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { FiPlus } from "react-icons/fi";
 import { MinusIcon } from '@heroicons/react/24/solid'
 import { useReducer } from 'react'
 
 function reducer(count, action) {
-  return count - 1
+  switch(action.type){
+    case 'increment':
+      return count +1
+      case 'decrement':
+        return count-1
+        default:
+          count
+        }
 }
 
 /// Tamamen fonksiyonel bir counter oluşturun
@@ -20,9 +28,16 @@ export default function CountUp() {
           onClick={() => dispatch({ type: 'increment' })}
           className='inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
         >
-          <MinusIcon className='-ml-0.5 mr-1.5 h-5 w-5' />1
+          <FiPlus  className='-ml-0.5 mr-1.5 h-5 w-5' />1
+        </button>
+        <button
+          onClick={() => dispatch({ type: 'decrement' })}
+          className='  inline-flex items-center rounded-md bg-red-600 px-3 py-2  font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600'
+        >
+          <MinusIcon className='ml-1 mr-1.5 h-5 w-5' />1
         </button>
       </div>
+      
     </div>
   )
 }
